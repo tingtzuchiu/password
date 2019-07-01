47b2d8af5a89e8581f3a402bd8c3d89f1e0f3623
Claire Chiu <tingtzuchiu@gmail.com>
2019-07-01 14:46:30 -0700

    new file

---
+++ password.py
@@ -0,0 +1,17 @@
+#reset password
+password = "a123456"
+i = 3 #剩餘機會
+while True:
+	pwd = input("輸入密碼: ")
+	if pwd == password:
+		print("登入成功")
+		break # 逃出迴圈
+	else:
+		i = i - 1
+		print("密碼錯誤！還有", i ,"次機會")
+		if i == 0:
+			print("帳號已鎖住")
+			break
+	
+		
+
